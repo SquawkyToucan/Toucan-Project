5ac1c75db58b16c16996577acbcc2f442b5da016
SquawkyToucan <league@WorthyMini-6.attlocal.net>
2017-04-28 17:23:51 -0700

    Began Level Two Project
    I’m not really sure what this will turn out to be, all I know is that
    it will involve toucans. Working on GUI now

---
+++ src/Stuff/Toucans.java
@@ -0,0 +1,16 @@
+package Stuff;
+
+import java.awt.Frame;
+
+import javax.swing.JFrame;
+
+public class Toucans {
+	JFrame frame = new JFrame();
+	Toucans() {
+		frame.setSize(500, 800);
+		frame.setVisible(true);
+	}
+	public static void main(String[] args) {
+		Toucans runner = new Toucans();
+	}
+}
