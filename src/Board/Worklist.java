af678b769179910878ac6cc1b7a2ec65afbcb38a
La Liga de Programadores Increibles <ewbrowne743@gmail.com>
2017-05-26 17:52:54 -0700

    Created a Class to Keep Track of Things to Do
    Here it is. Pretty lame on my part…

---
+++ src/Board/Worklist.java
@@ -0,0 +1,8 @@
+package Board;
+
+public class Worklist {
+	/**
+	 * Make buttons show image Give buttons a way to identify with a certain team Create frame with stuff that can be
+	 * used to manipulate other frame Make it pretty Add sound?
+	 */
+}
