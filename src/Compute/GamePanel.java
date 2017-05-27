3307b4604615c239dc598118afdbaf9370d39f9c
La Liga de Programadores Increibles <ewbrowne743@gmail.com>
2017-05-26 17:42:09 -0700

    Actually Managed to Get Buttons to Work
    Hooray!
    
    Thanks to the wonders of GridLayout…
     - Screen actually works
     - Control panel where you select moves, etc. will be migrated to a
    new, smaller frame that will have buttons a’plenty
     - More to come because I can actually work on this project now
    
    YAYAYAYA SUMMER VACATION IM SAVED
    
    Yashwin is not a swanki-swankster

--- src/Compute/GamePanel.java
+++ src/Compute/GamePanel.java
@@ -3,31 +3,29 @@ package Compute;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
-import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
-import java.awt.image.BufferedImage;
 
-import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
-public class GamePanel extends JPanel implements ActionListener, KeyListener, MouseListener{
+public class GamePanel extends JPanel implements ActionListener, KeyListener, MouseListener {
 	Timer timer;
-	JPanel playpanel;
-	JButton[] buttons = new JButton[35];
 	Font big = new Font("Menlo", Font.BOLD, 36);
 	Font small = new Font("Menlo", Font.PLAIN, 24);
+
 	GamePanel() {
-		timer = new Timer(1000/60, this);
+		timer = new Timer(1000 / 60, this);
 	}
+
 	void startGame() {
 		timer.start();
 	}
+
 	public void paintComponent(Graphics g) {
 		g.setColor(new Color(0, 153, 85));
 		g.fillRect(0, 0, Toucans.width, Toucans.height);
@@ -35,62 +33,54 @@ public class GamePanel extends JPanel implements ActionListener, KeyListener, Mo
 		g.setFont(big);
 		g.drawString("Creative Title", 150, 100);
 		g.setFont(small);
-		g.drawString("Click to Start Game", 275, 300);
+		g.drawString("Click to Start", 275, 300);
 	}
-	public void createBoard() {
-		int locx = 0;
-		int locy = 0; 
-		playpanel = new JPanel();
-		this.add(playpanel);
-		playpanel.addMouseListener(this);
-		for(int i = 0; i < 35; i++) {
-			buttons[i].setSize(100, 100);
-			buttons[i].setLocation(locx * 100, locy * 100 + 100);
-			locx++;
-			if(locx == 6) {
-				locy++;
-				locx = 0;
-			}
-			this.add(buttons[i]);
-		}
-	}
+
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		repaint();
 	}
+
 	@Override
 	public void keyTyped(KeyEvent e) {
 		System.out.println("Key typed");
 	}
+
 	@Override
 	public void keyPressed(KeyEvent e) {
 		System.out.println("Key pressed");
 	}
+
 	@Override
 	public void keyReleased(KeyEvent e) {
 		System.out.println("Key released");
 	}
+
 	@Override
 	public void mouseClicked(MouseEvent e) {
-		
+
 	}
+
 	@Override
 	public void mousePressed(MouseEvent e) {
-		
+
 	}
+
 	@Override
 	public void mouseReleased(MouseEvent e) {
-		//¡Hola! ¿Como estas? Muy bien, gracias. De nada. Hallo! Wie geht es dir? Mir gehts gut, danke! Bitte schon. 
+		// ¡Hola! ¿Como estas? Muy bien, gracias. De nada. Hallo! Wie geht es dir? Mir gehts gut, danke! Bitte schon.
 	}
+
 	@Override
 	public void mouseEntered(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
+
 	@Override
 	public void mouseExited(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 }
-//김종은임니다
+// 김종은임니다
--- src/Compute/Toucans.java
+++ src/Compute/Toucans.java
@@ -1,8 +1,10 @@
 package Compute;
 
+import java.awt.GridLayout;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
@@ -11,30 +13,61 @@ public class Toucans implements MouseListener {
 	// You can win through DOMINATION (35 tiles), TECH (35 points), ECONOMY (35K GDP + dependants, trade), or CIVILIZING
 	// Domination Victory: Every tile on the board is conquered. Enemies are vanquished.
 	// Technological Victory: 35 points worth of breakthrough, such as rockets, boats, etc. Can benefit military points
-	// Economical Victory: A combined sum of trade (per turn), subsidies to other tribes (per turn), production per turn, and GDP over 35K. On hard, if you near this all other countries will sanction you.
-	// Civilizing Victory: Your society has a combination of currency (5p), trade relations (5p), healthcare (5p), internet (10p - from Tech), education (5p), mining (5p), a university (5p), literature (5p), language (10p), and a city all combined to over 35 points + a population over 1,000
+	// Economical Victory: A combined sum of trade (per turn), subsidies to other tribes (per turn), production per
+	// turn, and GDP over 35K. On hard, if you near this all other countries will sanction you, meaning inner country
+	// must control majority of resources
+	// Civilizing Victory: Your society has a combination of currency (5p), trade relations (5p), healthcare (5p),
+	// internet (10p - from Tech), education (5p), mining (5p), a university (5p), literature (5p), language (10p), and
+	// a city all combined to over 35 points + a population over 1,000
 	// You can die through being CONQUERED or CONCEDING (lit lit lit lit lit)
 	// Death through Conquered: You are taken over by another tribe.
 	// Death through Starvation: Not enough food!
 	// Death through Lack of Economy: Your economy and currency collapse - anarchy happens.
 	// Conceding/giving up - Button will be available for this if necessary
+	JButton[] buttons = new JButton[36];
 	JFrame startframe = new JFrame();
 	JPanel startpanel = new JPanel();
+	JPanel playpanel = new JPanel();
 	JFrame game = new JFrame();
 	GamePanel gamePanel;
 	public static final int width = 600;
 	public static final int height = 900;
+
 	Toucans() {
 		startframe.setSize(width, height);
 		startframe.setVisible(true);
 		game.setSize(600, 900);
-		//Use like this - top 100 = Players, points
-		//next 600 down is game board (100x100) per piece
-		//(use JPanel and an image, then determine which block it is based off that)
-		//last 200 is bar with stuff like currency, and places to use troops and cut deals
+		// Use like this - top 100 = Players, points
+		// next 600 down is game board (100x100) per piece
+		// (use JPanel and an image, then determine which block it is based off that)
+		// last 200 is bar with stuff like currency, and places to use troops and cut deals
 		gamePanel = new GamePanel();
 		setup();
 	}
+
+	public void createBoard() {
+		int locx = 0;
+		int locy = 0;
+		JFrame frame = new JFrame();
+		frame.add(playpanel);
+		frame.setSize(600, 600);
+		playpanel.addMouseListener(this);
+		playpanel.setLayout(new GridLayout(6, 6));
+		for (int i = 0; i < 36; i++) {
+			buttons[i] = new JButton();
+			buttons[i].setSize(100, 100);
+			buttons[i].setLocation(locx * 100, locy * 100 + 100);
+			buttons[i].setVisible(true);
+			locx++;
+			if (locx == 6) {
+				locy++;
+				locx = 0;
+			}
+			playpanel.add(buttons[i]);
+		}
+		frame.setVisible(true);
+	}
+
 	void setup() {
 		startframe.add(gamePanel);
 		startframe.addMouseListener(this);
@@ -43,35 +76,37 @@ public class Toucans implements MouseListener {
 		game.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		startframe.addKeyListener(gamePanel);
 	}
+
 	public static void main(String[] args) {
 		Toucans runner = new Toucans();
 	}
-	public void play() {
-		game.add(gamePanel);
-		gamePanel.createBoard();
-	}
+
 	@Override
 	public void mouseClicked(MouseEvent e) {
-		gamePanel.createBoard();
+		createBoard();
 	}
+
 	@Override
 	public void mousePressed(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
+
 	@Override
 	public void mouseReleased(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
+
 	@Override
 	public void mouseEntered(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
+
 	@Override
 	public void mouseExited(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 }
